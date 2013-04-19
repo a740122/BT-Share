728a31c1991def91ef76d0f3ac862ace8fa159aa
zhkzyth <zhkzyth@gmail.com>
2013-04-19 19:01:42 +0800

    restruct the crawler

--- crawler/application.py
+++ crawler/application.py
@@ -1,101 +1,20 @@
 #coding:utf8
-import re
 from datetime import datetime
-from threading import Thread
-from bs4 import BeautifulSoup
-from hashlib import md5
 from Queue import Queue
+import os
+import traceback
+CURRENT_DIR = os.path.dirname(os.path.realpath(__file__))
 
 from crawler import Crawler
 from database import Database
 from logmanager import LogManager
-from libs import util
-
-import os
-import sys
-import traceback
-PARENT_ROOT = os.path.dirname(os.path.realpath(__file__))
-
 import cus_mail
+import util
+
 
 class HTMLParseException(Exception):
     def __init__(self):
         Exception.__init__(self)
-
-
-def mininova_spider():
-    def callback(webPage):
-        url, pageSource = webPage.getDatas()
-        soup = BeautifulSoup(pageSource)
-        #tmp var
-        _ = ''
-        #more robust
-        param = {}
-        #get id
-        param['id'] = md5(url).hexdigest(),
-        #get url
-        param['url'] = url,
-        #get name
-        try:
-            _ = soup.find(id="content").h1.string
-        except:
-            _ = 'unknown'
-        finally:
-            param['name'] = _
-        #get size
-        try:
-            _ = soup.find(
-                id='specifications'
-                ).find_all("p")[2].get_text().strip().split('\n')[1].replace(u'\xa0', u' '),
-        except:
-            _ = 'unknown'
-        finally:
-            param['size'] = _
-        #get description
-        try:
-            _ = re.compile(r'[\n\r\t]').sub(
-                " ",soup.find(id='description').get_text()),
-        except:
-            _ = 'not description right now~XD'
-        finally:
-            param['description'] = _
-        #get magnet_link
-        try:
-            _ = soup.find(id="download").find_all("a")[2]['href']
-        except:
-            #drop it or redo?
-            return
-        else:
-            param['magnet_link'] = _
-        query = {"id": param['id']}
-        database = Database(db="bt_tornado")
-        database.saveData(collection='seed', query=query, document=param)
-
-    #args to init spider
-    entryFilter = dict()
-    entryFilter['Type'] = 'allow'
-    entryFilter['List'] = [r'/tor/\d+', r'/today', r'/yesterday', r'/sub/\d+']
-
-    yieldFilter = dict()
-    # yieldFilter['Type'] = 'allow'
-    # yieldFilter['List'] = [r'$']
-    callbackFilter = dict()
-    callbackFilter['List'] = [r'/tor/\d+', ]
-    callbackFilter['func'] = callback
-
-    args = dict(
-        url=['http://www.mininova.org/today',],
-        depth=1,
-        threadNum=1,
-        keyword='',
-        entryFilter=entryFilter,
-        yieldFilter=yieldFilter,
-        callbackFilter=callbackFilter,
-        db='bt_tornado',
-        collection='link2search',
-    )
-
-    return args
 
 
 class SpiderManager(object):
@@ -124,7 +43,7 @@ class SpiderManager(object):
             self.logger.error("spider cannot run.")
         finally:
             seed_num = self.database.db['seed'].count()
-            textfile = PARENT_ROOT + '/log/spider.log'
+            textfile = CURRENT_DIR + '/log/spider.log'
             self.logger.info("now your seeds num is %s." % seed_num)
             try:
                 fp = open(textfile, 'rb')
---
+++ crawler/mail_config.py
@@ -0,0 +1,7 @@
+gmail_config = {
+        'mail_host': "smtp.gmail.com",
+        'mail_port': 25,
+        'mail_user': "zhkzyth",
+        'mail_pass': "zhg709394",
+        'mail_postfix': "gmail.com",
+     }
